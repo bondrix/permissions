941db825639f9e24a304e12263eb83773def1f3c
slowmarc <markos.yiakoumis@icloud.com>
2025-01-07 19:03:30 +0000

    Update structure

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Bondrix'
+version '1.0.0'
+
+client_script ''
+server_script {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua',
+    'server/functions.lua',
+    'server/commands.lua'
+}
+shared_script ''
+
+dependencies 'oxmysql'
